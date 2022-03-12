2f21fddc5155b53b65dd5cd61f5dbd276ce4ac3d
Sahib111 <58338778+Sahib111@users.noreply.github.com>
2022-03-12 12:26:39 +0530

    Create main1.js

---
+++ js/main1.js
@@ -0,0 +1,28 @@
+//<![CDATA[
+      imgr=new Array();
+      imgr[0]="http://3.bp.blogspot.com/-zP87C2q9yog/UVopoHY30SI/AAAAAAAAE5k/AIyPvrpGLn8/s1600/picture_not_available.png";
+      showRandomImg=true;
+      aBold=true;
+      summaryPost=150;
+      summaryPost1=0;
+      summaryTitle=15;
+      numposts=12;
+      numposts1=6;
+      numposts2=6;
+      numposts3=6;
+      numposts4=6;
+      numposts5=12;
+      numposts6=1;
+	numposts23=10;
+postnum1=3;
+postnum2=6;
+postnum3=5;
+postnum4=3;
+postnum5=2;
+postnum6=5;
+postnum7=3;
+postnum8=8;
+      function removeHtmlTag(strx,chop){var s=strx.split("<");for(var i=0;i<s.length;i++){if(s[i].indexOf(">")!=-1){s[i]=s[i].substring(s[i].indexOf(">")+1,s[i].length)}}s=s.join("");s=s.substring(0,chop-1);return s}
+
+
+//]]>
