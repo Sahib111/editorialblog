7c13d49edf9829235ac4a58fc0395e449a8ef81a
Sahib111 <58338778+Sahib111@users.noreply.github.com>
2022-03-12 15:33:37 +0530

    Create main13.js

---
+++ js/main13.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($) {
+ $('#megamenuid').megaBloggerMenu({
+  postsNumber : 4, 
+  noThumbnail : 'https://2.bp.blogspot.com/-Z7jG9iO9OTg/Vazz6YJnFQI/AAAAAAAALVs/wlSvXPgdDAo/s1600/no_image_available.png' 
+ });
+});
