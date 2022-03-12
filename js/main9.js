c9f06cdc5fefa3fcc3434e2e8e836750a75fa513
Sahib111 <58338778+Sahib111@users.noreply.github.com>
2022-03-12 15:23:31 +0530

    Create main9.js

---
+++ js/main9.js
@@ -0,0 +1,7 @@
+var postperpage=12;
+          var numshowpage=4;
+          var upPageWord=" < ";
+          var downPageWord=" > ";
+          var home_page="/";
+          var urlactivepage=location.href;
+       
