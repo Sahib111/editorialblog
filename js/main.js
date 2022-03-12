3e37c624f1acce1799c2c2fe2dcd3e1436125a77
Sahib111 <58338778+Sahib111@users.noreply.github.com>
2022-03-12 15:16:56 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var sliders = 'slider2';
+			if(sliders=='slider1'||sliders=='Slider1'||sliders=='SLIDER1'){
+				$('body').addClass('slider1');                
+			}
+            else if(sliders=='slider2'||sliders=='Slider2'||sliders=='SLIDER2'){
+				$('body').addClass('slider2');                
+			}		
+            else{
+                $('body').addClass('slider1');                
+            }
