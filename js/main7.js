361cee0fa7e940eae6095e4f34c605f02fc4ed8f
Sahib111 <58338778+Sahib111@users.noreply.github.com>
2022-03-12 15:16:07 +0530

    Create main7.js

---
+++ js/main7.js
@@ -0,0 +1,7 @@
+var hover = 'on';
+			if(hover=='on'||hover=='On'||hover=='ON'){
+				$('body').addClass('hoveron');                
+			}
+			else{
+				$('body').addClass('hoveroff');                
+			}
