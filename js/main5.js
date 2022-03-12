c7501e7515892ecf1d7f6a10661b7987f8de952f
Sahib111 <58338778+Sahib111@users.noreply.github.com>
2022-03-12 15:00:47 +0530

    Create main5.js

---
+++ js/main5.js
@@ -0,0 +1,7 @@
+var preloader = 'on';
+			if(preloader=='on'||preloader=='ON'||preloader=='On'){
+				$('body').addClass('preloader-on');                
+			}
+            else {
+	          	$('body').addClass('preloader-off');
+            }		
